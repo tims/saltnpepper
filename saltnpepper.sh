4dd9685c8bac3abdee5df6719fe87fa26c363878
Tim Sell <tim@songkick.com>
2011-09-26 15:00:49 +0100

    use quotes

--- saltnpepper.sh
+++ saltnpepper.sh
@@ -5,8 +5,8 @@ EDITOR=mate
 USERNAME=`whoami`
 AUTHOR=`finger $USERNAME | grep Name | awk -F "Name: " '{print $2}'`
 
-if [ -d $1 ]; then
-  HOME=$1
+if [ -d "$1" ]; then
+  HOME="$1"
 else
   echo "directory $1 does not exist"
   exit 1
@@ -18,35 +18,35 @@ else
   DATE=`date "+%Y.%m.%d"`
 fi
 
-HOME=$1
+HOME="$1"
 NAME=`basename $HOME`
 MASTER_FILE=$HOME/$NAME.rst
 FILE=$HOME/$DATE.rst
 
-if [ -f $FILE ]; then
+if [ -f "$FILE" ]; then
   echo $FILE exists
 else
   echo $FILE does not exist, starting new one.
-  echo $DATE >> $FILE
-  echo ========== >> $FILE
-  echo >> $FILE
+  echo $DATE >> "$FILE"
+  echo ========== >> "$FILE"
+  echo >> "$FILE"
 fi
 
 
-if [ -f $MASTER_FILE ]; then
-  rm $MASTER_FILE
+if [ -f "$MASTER_FILE" ]; then
+  rm "$MASTER_FILE"
 fi
 
-echo ":Title: $NAME" >> $MASTER_FILE
-echo ":Author: $AUTHOR" >> $MASTER_FILE
-echo ":Date: $DATE" >> $MASTER_FILE
-echo >> $MASTER_FILE
+echo ":Title: $NAME" >> "$MASTER_FILE"
+echo ":Author: $AUTHOR" >> "$MASTER_FILE"
+echo ":Date: $DATE" >> "$MASTER_FILE"
+echo >> "$MASTER_FILE"
 
-for rstfile in `ls $HOME | grep .rst | grep -v $NAME.rst | sort`
+for rstfile in `ls $HOME | grep .rst | grep -v "$NAME.rst" | sort`
 do
-  echo ".. include:: $rstfile" >> $MASTER_FILE
+  echo ".. include:: $rstfile" >> "$MASTER_FILE"
 done
 
-$EDITOR $FILE
+$EDITOR "$FILE"
 
-rst2pdf $MASTER_FILE
+rst2pdf "$MASTER_FILE"
